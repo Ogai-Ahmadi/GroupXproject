4d229447c2aecf046ec9aa8bbf9c4f961c626c70
Ogai-Ahmadi <106423615+Ogai-Ahmadi@users.noreply.github.com>
2022-09-01 11:15:37 -0700

    Karla edited her own Karla class

--- src/main/Karla.java
+++ src/main/Karla.java
@@ -3,5 +3,9 @@ package main;
 public class Karla {
     public static void main(String[] args){
         System.out.println("This line is added by karla");
+        ////////////Karla added this information/////////
+        int id = 12345;
+        String name = "Karla";
+        System.out.println(id + name);
     }
 }
